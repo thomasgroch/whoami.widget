8aaddc6cfd254d81d1a8f2c3660611298c99e701
Thomas Groch <thomas.groch@gmail.com>
2019-11-07 17:10:41 -0300

    project renamed
    from app.widget to workana.widget

--- index.js
+++ index.js
@@ -2,26 +2,26 @@
 command: "curl -s http://localhost:3000/search | /usr/local/bin/jq '.data'",
 refreshFrequency: 5000,
 render: function (input) {
-    window.appWidget = {
+    window.workanaWidget = {
         input: input
     };
-    return '<div id="app-widget"></div>';
+    return '<div id="workana-widget"></div>';
 },
 afterRender: function(domEl) {
     var Vue = require('./node_modules/vue/dist/vue.js');
-    var AppWidget = require('./lib/AppWidget');
+    var WorkanaWidget = require('./lib/WorkanaWidget');
     var app = new Vue({
-        el: '#app-widget',
-        template: '<app-widget id="app-widget" v-bind:input="input"></app-widget>',
+        el: '#workana-widget',
+        template: '<workana-widget id="workana-widget" v-bind:input="input"></workana-widget>',
         data: function() {
-            return window.appWidget
+            return window.workanaWidget
         },
         components: {
-            AppWidget: AppWidget
+            WorkanaWidget: WorkanaWidget
         }
     });
 },
 update: function(input) {
-    window.appWidget.input = input;
+    window.workanaWidget.input = input;
 },
-style: '@import "./app.widget/dist/output.css"',
+style: '@import "./workana-widget/dist/output.css"',
